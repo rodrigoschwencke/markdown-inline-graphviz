1e779a6069c4554dfe14b7d0d285496cf76f7f3d
rodrigoschwencke <83183950+rodrigoschwencke@users.noreply.github.com>
2021-04-29 15:49:26 +0200

    Fix SVG Rendering

--- markdown_inline_graphviz.py
+++ markdown_inline_graphviz.py
@@ -18,7 +18,8 @@ filename specified in the tag. Example:
 Requires the graphviz library (http://www.graphviz.org/) and python 3
 
 Inspired by jawher/markdown-dot (https://github.com/jawher/markdown-dot)
-Forked from sprin/markdown-inline-graphviz (https://github.com/sprin/markdown-inline-graphviz
+Forked from  cesaremorel/markdown-inline-graphviz (https://github.com/cesaremorel/markdown-inline-graphviz)
+
 """
 
 import re
